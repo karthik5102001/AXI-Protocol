43d6dbb033272c856d5ced8cf2fcfdf2160b41b7
Karthik S <116733736+karthik5102001@users.noreply.github.com>
2025-07-06 03:16:05 +0530

    Round Robin Arbiter
    The AXI Round Robin Arbiter is a key component in AXI protocol-based systems, ensuring fair access to shared resources among multiple masters. It cycles through requesting masters in a circular manner, granting access based on a fixed priority sequence. This arbitration scheme prevents starvation, providing equitable bandwidth allocation for data transfers. It is commonly used in AXI interconnects to manage traffic in complex SoC designs.

---
+++ AXI_Stream/Arbiter/Round_Robin/DUT/AXI_RR_Arbiter.sv
@@ -0,0 +1,117 @@
+// Simulation : Done
+// Lint Check : Pending
+// STA Analysis : Pending
+// Synthesis : Pending
+// Description : AXI Round Robin Arbiter
+// Author : Karthik
+// Date : 2025-07-06
+
+
+module AXI_Arbiter (
+	input wire Aclk,
+	input wire Areset_n,
+	output reg s_axis_tready1, // output going to its respected master
+	output reg s_axis_tready2,
+	input wire s_axis_tvalid1,
+	input wire s_axis_tvalid2,
+	input wire s_axis_tlast1,
+	input wire s_axis_tlast2,
+	input wire [7:0] s_axis_tdata1,
+	input wire [7:0] s_axis_tdata2,
+
+	input wire m_axis_tready,
+	output reg m_axis_tlast,
+	output reg m_axis_tvalid,
+	output reg [7:0] m_axis_tdata
+);
+
+typedef enum bit [1:0] {idle = 2'b00, s1 = 2'b01, s2 = 2'b10} state_reg;
+state_reg state, next_state;
+
+assign s_axis_tready1 = 1'b1;  // we assign one to the master part for lower  
+assign s_axis_tready2 = 1'b1;  // the time compution
+
+always_ff @(posedge Aclk or negedge Areset_n)
+begin
+	if(!Areset_n)
+	begin
+		state <= idle;
+	end
+	else begin
+		state <= next_state;
+	end
+end
+
+reg [7:0] reg_data;
+reg reg_last;
+
+always_comb begin
+	case(state)
+			idle : begin
+					if(s_axis_tvalid1 && s_axis_tready1)begin
+						next_state  = s1;
+						reg_data = s_axis_tdata1;   // we will hold our data when we jump form 
+						reg_last = s_axis_tlast1;	// idle to S1
+					end
+					else if(s_axis_tvalid2 && s_axis_tready2)begin
+						next_state  = s2;
+						reg_data = s_axis_tdata2;  // we will hold our data when we jump form
+						reg_last = s_axis_tlast2;  // idle to S2
+					end
+					else next_state = idle;
+			end
+			s1 	: begin
+					if(m_axis_tready) begin
+						if(s_axis_tlast1) begin
+							reg_data = s_axis_tdata1;
+							reg_last = s_axis_tlast1;
+								if(s_axis_tvalid2 && s_axis_tready2)
+								begin
+									next_state = s2; // if we have responce from other master(M2)
+									end				 // then we go to S2
+								else begin
+									next_state = idle;
+									end
+							end
+						else begin
+							next_state = s1;
+							reg_data = s_axis_tdata1;
+							reg_last = s_axis_tlast1;
+								end
+						end
+				else next_state = s1;
+					end
+		
+				s2 	: begin
+					if(m_axis_tready) begin
+						if(s_axis_tlast2) begin
+							reg_data = s_axis_tdata2;
+							reg_last = s_axis_tlast2;
+								if(s_axis_tvalid1 && s_axis_tready1)
+								begin
+									next_state = s1;// if we have responce from other master(M2)
+								end					// then we go to S2
+								else begin
+									next_state = idle;
+								end
+							end
+						else begin
+							next_state = s2;
+							reg_data = s_axis_tdata2;
+							reg_last = s_axis_tlast2;
+								end
+						end
+				else next_state = s2;
+					end
+	
+		 default : begin
+					next_state = idle;
+		 end
+		endcase			
+		end
+
+assign m_axis_tdata  = ((s_axis_tvalid1 && s_axis_tready1)||(s_axis_tvalid2 && s_axis_tready2)) ? reg_data : 8'h00;
+assign m_axis_tvalid = ((s_axis_tvalid1 && s_axis_tready1)||(s_axis_tvalid2 && s_axis_tready2)) ? 1'b1 : 1'b0;
+assign m_axis_tlast  = ((s_axis_tvalid1 && s_axis_tready1)||(s_axis_tvalid2 && s_axis_tready2)) ? reg_last : 1'b0;
+
+endmodule
---
+++ AXI_Stream/Arbiter/Round_Robin/TB/AXI_RR_Arbiter_TB.sv
@@ -0,0 +1,153 @@
+
+module Arbiter_TB();
+
+	parameter Delay = 5;
+
+	 reg Aclk;
+	 reg Aresetn;
+	 wire s_axis_tready1; // output going to its respected master
+	 wire s_axis_tready2;
+	 reg s_axis_tvalid1;
+	 reg s_axis_tvalid2;
+	 reg s_axis_tlast1;
+	 reg s_axis_tlast2;
+	 reg [7:0] s_axis_tdata1;
+	 reg [7:0] s_axis_tdata2;
+
+     reg m_axis_tready;
+	 wire m_axis_tlast;
+	 wire m_axis_tvalid;
+	 wire [7:0] m_axis_tdata;
+
+
+
+AXI_Arbiter DUT(Aclk,Aresetn,s_axis_tready1,s_axis_tready2,s_axis_tvalid1,s_axis_tvalid2, s_axis_tlast1,s_axis_tlast2,s_axis_tdata1,s_axis_tdata2,m_axis_tready,m_axis_tlast,m_axis_tvalid, m_axis_tdata);
+
+
+	initial begin
+		Aclk <= 1'b0;
+		Aresetn <= 1'b0;
+		repeat(5) @(posedge Aclk);
+		Aresetn <= 1'b1;
+		repeat(5) @(posedge Aclk);
+
+		/// Master 1
+
+		s_axis_tvalid1 <= 1'b1;
+		s_axis_tvalid2 <= 1'b0;
+
+		repeat(3)@(posedge Aclk);
+		m_axis_tready <= 1'b1;
+
+		repeat(5) begin
+		@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b1;
+		s_axis_tvalid2 <= 1'b0;
+		s_axis_tdata1 <= $urandom_range(0,31);
+		s_axis_tdata2 <= $urandom_range(0,31);
+		s_axis_tlast1 <= 1'b0;
+		s_axis_tlast2 <= 1'b0;
+				   end
+		@(posedge Aclk);
+		s_axis_tdata1 <= $urandom_range(0,31);
+		s_axis_tlast1 <= 1'b1;
+		@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b0;
+		s_axis_tlast1  <= 1'b0;
+//		m_axis_tready <= 1'b0; // dont off 
+
+		/// Master 2
+
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b1;
+		repeat(3) @(posedge Aclk);
+		m_axis_tready <= 1'b1;
+
+		repeat(5) begin
+			@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b1;
+		s_axis_tdata1 <= $urandom_range(31,51);
+		s_axis_tdata2 <= $urandom_range(31,51);
+		s_axis_tlast1 <= 1'b0;
+		s_axis_tlast2 <= 1'b0;
+				   end
+		@(posedge Aclk);
+		s_axis_tdata2 <= $urandom_range(31,51);
+		s_axis_tlast2 <= 1'b1;
+		@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b0;
+		s_axis_tlast2  <= 1'b0;
+		m_axis_tready <= 1'b0;
+
+		/// Master 1 and Master 2 same time (if M1 get selected try next M2)
+
+		repeat(5) @(posedge Aclk);
+
+		s_axis_tvalid1 <= 1'b1;
+		s_axis_tvalid2 <= 1'b1;
+
+		repeat(3) @(posedge Aclk);
+		m_axis_tready <= 1'b1;
+
+		repeat(5) begin
+			@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b1;
+		s_axis_tvalid2 <= 1'b1;
+		s_axis_tdata1 <= $urandom_range(31,51);
+		s_axis_tdata2 <= $urandom_range(31,51);
+		s_axis_tlast1 <= 1'b0;
+		s_axis_tlast2 <= 1'b0;
+				   end
+		@(posedge Aclk);
+		s_axis_tdata1 <= $urandom_range(31,51);
+		s_axis_tlast1 <= 1'b1;
+		@(posedge Aclk);
+
+		///////////////////////////////// send M2 again
+
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b1;
+
+		repeat(3) @(posedge Aclk);
+		m_axis_tready <= 1'b1;
+
+		repeat(5) begin
+		@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b1;
+		s_axis_tdata1 <= $urandom_range(31,51);
+		s_axis_tdata2 <= $urandom_range(31,51);
+		s_axis_tlast1 <= 1'b0;
+		s_axis_tlast2 <= 1'b0;
+				   end
+		@(posedge Aclk);
+		s_axis_tdata2 <= $urandom_range(31,51);
+		s_axis_tlast2 <= 1'b1;
+		@(posedge Aclk);
+		s_axis_tvalid1 <= 1'b0;
+		s_axis_tvalid2 <= 1'b0;
+		s_axis_tlast2  <= 1'b0;
+		m_axis_tready <= 1'b0;
+		
+		repeat(10) @(posedge Aclk);
+
+		$finish;
+	end
+
+	always #(Delay) Aclk = ~Aclk;
+
+	initial begin
+		$dumpfile("wave_vcd.vcd");
+		$dumpvars(0,Arbiter_TB);
+		end
+
+	initial begin
+		$dumpfile("wave_fsdb.fsdb");
+    	$dumpvars(0,Arbiter_TB);
+		end
+
+
+endmodule
